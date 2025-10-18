53770fa0c56e6af4301e3642019d16bbc9311cde
phoenixsigil68-ctrl <phoenixsigil68@gmail.com>
2025-10-18 17:32:37 +0530

    Getting started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
