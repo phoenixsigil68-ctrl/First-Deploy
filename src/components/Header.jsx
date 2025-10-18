706b6c73f19281f5d0aa2dcc7d14a8cd174524c2
phoenixsigil68-ctrl <phoenixsigil68@gmail.com>
2025-10-18 18:14:12 +0530

    Header section completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "../index.css";
-
+import { Link as LinkScroll } from "react-scroll";
 export const Header = () => {
-  return;
+  function NavLink({ title }) {
+    return <LinkScroll className="list">{title}</LinkScroll>;
+  }
+
+  return (
+    <header className="fixed top-0 left-0 right-0 w-full z-50 py-10">
+      <div className="flex justify-around items-center">
+        <NavLink title="Features" />
+        <div className="dots" />
+        <NavLink title="Pricing" />
+
+        <LinkScroll>
+          <img src="/images/xora.svg" width={135} height={55} />
+        </LinkScroll>
+
+        <NavLink title="Faq" />
+        <div className="dots" />
+        <NavLink title="Download" />
+      </div>
+    </header>
+  );
 };
